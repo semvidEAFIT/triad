76507b64b26b884da5497469a5992928bdbd26a4
darang22 <skap.mks@gmail.com>
2015-09-29 15:17:22 -0500

    Added the warrior attack script and the Attack test scene

---
+++ Assets/Warrior/Attack/SwordAttack.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class SwordAttack : MonoBehaviour, ISkill
+{
+    public Collider col;
+    public float swingTime = 4f;
+
+    public void Cast()
+    {
+        col.enabled = true;
+        StartCoroutine(TurnOffCollider());
+    }
+
+    private IEnumerator TurnOffCollider()
+    {
+        yield return new WaitForSeconds(swingTime);
+        col.enabled = false;
+    }
+}
