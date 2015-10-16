fb1780da6f6063aa1a6f3735e418d59c53e422f7
yampyer <yampyer.tr3s@gmail.com>
2015-10-16 01:07:53 -0500

    Added blinking animation when player get hurts

--- Assets/Warrior/Health.cs
+++ Assets/Warrior/Health.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 namespace Triad.Warriors
 {
@@ -7,10 +8,14 @@ namespace Triad.Warriors
 
         public float health = 100;
         public new Renderer renderer;
+        private bool isInvincible;
+        public float timeInvincible = 2f;
+
 
         private void Awake()
         {
             renderer = GetComponent<Renderer>();
+            isInvincible = false;
         }
 
         public float GetHeath()
@@ -24,6 +29,36 @@ namespace Triad.Warriors
             renderer.material.color = renderer.material.color + Color.red * (health / 100f);
             if (health <= 0)
                 Destroy(gameObject);
+        }
+
+        public void onHit(float damage) {
+            if (health > 0 && !isInvincible)
+            {
+                health = health - damage;
+                if (health <= 0)
+                    Destroy(gameObject);
+                isInvincible = true;
+                StartCoroutine(BlinkCharacter());
+                StartCoroutine(StopBlinking());
+            }
+        }
+
+        public IEnumerator BlinkCharacter()
+        {
+            while (isInvincible)
+            {
+                renderer.enabled = false;
+                yield return new WaitForSeconds(.1f);
+                renderer.enabled = true;
+                yield return new WaitForSeconds(.1f);
+            }
+        }
+
+        IEnumerator StopBlinking()
+        {
+            yield return new WaitForSeconds(timeInvincible);
+            isInvincible = false;
+            renderer.enabled = true;
         }
     }
 }
